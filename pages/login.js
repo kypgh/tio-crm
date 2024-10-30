740b09d5a3d767b7599847392e16709ef78631bd
kypgh <kypghlol@hotmail.com>
2024-10-30 11:25:27 +0200

    init

--- pages/login.js
+++ pages/login.js
@@ -14,7 +14,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import useTheme from "../utils/hooks/useTheme";
 import Logo from "../components/Logo";
 import useSelectedBrand from "../utils/hooks/useSelectedBrand";
-
+//
 const Outer = styled.div`
   display: flex;
   justify-content: center;
